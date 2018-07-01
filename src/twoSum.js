598167012cd4b227780db2c1d5df967d5698f3f2
Sheenan Tenepre <sheenantenepre@gmail.com>
2018-07-01 16:35:30 +0900

    two sum algo

---
+++ src/twoSum.js
@@ -0,0 +1,17 @@
+// find 2 values in the array that equates to sum value
+const twoSum = (numArray, sum) => {
+  const pairs = []
+  const hashTable = numArray
+
+  numArray.map(num => {
+    const counterPart = sum - num
+    // check if current number is already in the hashTable
+    if (hashTable.indexOf(counterPart) >= 0) {
+      pairs.push([num, counterPart])
+    }
+  })
+
+  return pairs
+}
+
+twoSum([1, 2, 3, 4, 5, 6, 2, 1, 3], 10)
